ff5f645a51e48953d92bbbac44e0b705de5bf83e
jooyoung91 <jooyoung91@gmail.com>
2016-05-22 00:55:49 -0700

    Use Express and set default page in the server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 //Modules
 var express = require('express');
-var bodyParser = require('body-parser');
-var multer  = require('multer');
-var fs = require("fs");
-var connect = require('connect');
+//var bodyParser = require('body-parser');
+//var multer  = require('multer');
+//var fs = require("fs");
 
-var app = connect();
-app.use(serveStatic("../AngularJS"));
-app.listen(8080);
+var app = express();
+
+var options = {
+  index: "index.htm"
+};
+
+app.use('/', express.static('../AngularJS', options));
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('my app is listening at http://%s:%s', host, port);
+});
