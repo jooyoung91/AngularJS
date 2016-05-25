f3a417f4e8e942d9a2ab32905e34dd8e1b9e1c59
jooyoung91 <jooyoung91@gmail.com>
2016-05-25 01:27:27 -0700

    sportsstore application

---
+++ controllers/productListController.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("sportsStore")
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount){
+        var selectedCategory = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
+        $scope.selectCategory = function(newCategory) {
+            selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectPage = function(newPage){
+            $scope.selectedPage = newPage;
+        }
+        $scope.categoryFilterFn = function(data) {
+            return selectedCategory == null || data["category"] == selectedCategory;
+        }
+
+        $scope.getCategoryClass = function(category){
+            if(selectedCategory == category)
+                return productListActiveClass
+            else
+                return "";
+        }
+
+        $scope.getPageClass = function(page) {
+            return page == $scope.selectedPage ? productListActiveClass : "";
+        }
+    })
