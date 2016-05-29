d2b6befe773b657cd67afdb00c7c00d35fd97fa2
jooyoung91 <jooyoung91@gmail.com>
2016-05-28 21:34:27 -0700

    Chapter12

---
+++ controllers/controllers.js
@@ -0,0 +1,8 @@
+var controllerModule = angular.module("exampleApp.Controllers", [])
+
+controllerModule.controller("dayCtrl", function($scope, days) {
+    $scope.day = days.today;
+});
+controllerModule.controller("tomorrowCtrl", function($scope, days) {
+    $scope.day = days.tomorrow;
+});
